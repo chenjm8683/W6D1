8954309a95aae50f2526a55062a0b0fe0f5dc1ad
Jingming Chen <chenjm8683@gmail.com>
2015-11-30 14:11:27 -0800

    Inherits

---
+++ inherits.js
@@ -0,0 +1,8 @@
+
+Function.prototype.inherits = function (superClass) {
+  var Surrogate = function () {};
+  Surrogate.prototype = superClass.prototype;
+
+  this.prototype = new Surrogate();
+  this.prototype.constructor = this;
+};
